3a0e07ee8ac3a81d2bde4f18fd4aaf811f8ed9fa
umarahad2005 <umarahadusmani@gmail.com>
2026-01-16 16:17:24 +0500

    hmm

--- lab14/backend/server.js
+++ lab14/backend/server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/ecomcrud")
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
 const productRoutes = require("./routes/productRoutes");
 app.use("/products", productRoutes);
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
